42b4cc9abdf4d8044799e151afe46aa9120c3c23
OthmaneAAINANI <76712196+OthmaneAAINANI@users.noreply.github.com>
2020-12-29 02:00:18 +0100

    Update testchild.py

--- testchild.py
+++ testchild.py
@@ -1,2 +1,3 @@
 # Adding a new file in the child branch
 print("inside child branch")
+https://www.coursera.org/learn/open-source-tools-for-data-science/ungradedWidget/37d1L/pre-requisites-for-command-line-interface-optional
